0c7641dfcc8d358bb171d37867b402720bce9b71
Mlab Root <mazerj2006@gmail.com>
2006-04-13 18:12:20 +0000

    - sanity check for pypeboot to make sure user has root-access.   If you don't, pype won't start -- this is to avoid hard to   track down IPC/SHM problems (that bit us on the ass this   morning).
    git-svn-id: svn+ssh://svn/auto/share/repos/LOCKDOWN/mlab-pype/pype2/trunk@169 97a921b0-85fb-0310-a969-e0731ffd8486

--- src/wrapper/pypeboot.c
+++ src/wrapper/pypeboot.c
@@ -3,5 +3,10 @@
 
 main(int ac, char **av)
 {
-  execvp(av[1], av+1);
+  if (geteuid() != 0) {
+    fprintf(stderr, "pypeboot: not suid root and not running as root.\n");
+    exit(1);
+  } else {
+    execvp(av[1], av+1);
+  }
 }
