dd498a1d716f83e0b24c3261c3f49c72cfd57dfa
Mlab Root <mazerj2006@gmail.com>
2010-04-02 14:47:02 +0000

    - make p2mset use filesystem to store <var,val> pairs persistently   so they're sticky from session to session.... use with caution!
    git-svn-id: svn+ssh://svn/auto/share/repos/LOCKDOWN/mlab-pype/pype2/trunk@381 97a921b0-85fb-0310-a969-e0731ffd8486

--- src/p2m/p2mset.m
+++ src/p2m/p2mset.m
@@ -1,35 +1,71 @@
 function val = p2mset(var, val)
+%function val = p2mset(var, val)
 %
-% persistent name-value storage pairs (for fixed parameters)
+% Persistent name-value storage pairs (aka dictionary). Pairs
+% are persistently stored in a file (~/.p2mset), which means
+% this is not a fast function. Use with caution in speed critical
+% code!
+%
+% INPUT
+%   p2mset               - print table of values
+%   val = p2mset(var)    - lookup value ([] for undefined)
+%   p2mset(var, val)     - set var to val (returns val)
+%   p2mset(var, [])      - clear/delete var
+%   p2mset('!clearall')  - clear all vars
+%
+% OUTPUT
+%   val - value (if any) of the named var -- [] for undefined.
 %
 %Wed Mar 31 14:05:35 2010 mazer -- created
 
-persistent ptab
-
-if isempty(ptab)
-  ptab.vars = {};
-  ptab.vals = {};
+fname = [getenv('HOME') '/.p2mset'];
+if exist(fname, 'file')
+  load('-mat', fname);
+else
+  tab.vars = {};
+  tab.vals = {};
 end
 
 if nargin == 0
-  for n = 1:length(ptab.vars)
-    disp({ptab.vars{n} ptab.vals{n}});
+  % display table
+  disp({'NAME' 'VALUE'});
+  disp([tab.vars' tab.vals']);
+  return;
+elseif nargin > 0 && strcmp(var, '!clearall')
+  tab.vars = {};
+  tab.vals = {};
+  if exist(fname, 'file')
+    delete(fname);
   end
-  val = [];
-  return
+  return;
 end
 
-n = strmatch(var, ptab.vars);
+% find entry if table, if var exists
+n = strmatch(var, tab.vars);
+
 if isempty(n)
+  % new var, create entry, unless val is []
   if nargin == 1
     val = [];
     return;
   end
-  n = length(ptab.vars)+1;
-  ptab.vars{n} = var;
+  n = length(tab.vars)+1;
+  tab.vars{n} = var;
+  tab.vals{n} = 'never-set';
 end
 
 if nargin==2
-  ptab.vals{n} = val;
+  if isempty(val)
+    % delete var from table
+    ix = 1:length(tab.vars);
+    ix = find(ix ~= n);
+    tab.vars =  tab.vars(ix);
+    tab.vals =  tab.vals(ix);
+    val = [];
+  else
+    tab.vals{n} = val;
+  end
+  save(fname, 'tab');
+else
+  val = tab.vals{n};
 end
-val = ptab.vals{n};
